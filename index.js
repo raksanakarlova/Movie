fea157bb37651b6328106b2d3b31713a1d73f850
Raksana Karlova <raksana.karlova@gmail.com>
2024-01-14 17:14:29 +0400

    практическое задание 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы посмотрели в этом году?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели в этом году?', '');
+
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -10,12 +11,27 @@ const personalMovieDB = {
 	privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-	b = prompt('На сколько вы оцените этот фильм?', ''),
-	c = prompt('Один из последних просмотренных фильмов?', ''),
-	d = prompt('На сколько вы оцените этот фильм?', '');
+if(personalMovieDB.count < 10) {
+	alert('Просмотрено довольно мало фильмов');
+} else if(personalMovieDB.count >= 10 || personalMovieDB.count < 30) {
+	alert('Вы классический зритель');
+} else if(personalMovieDB.count > 30){
+	alert('Вы киноман');
+} else {
+	alert('Произошла ошибка');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for(let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+		b = prompt('На сколько вы оцените этот фильм?', '');
+
+	if(a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log('done');
+	} else {
+		console.log('error');
+		i--;
+	}
+}
 
 console.log(personalMovieDB);
